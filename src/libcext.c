41bc5223d753e80b9af0b10de7b90d234f397573
Guillaume PELLEGRINO <guillaume.pellegrino@yahoo.com>
2022-11-03 16:27:42 +0100

    Implement strncmp() and strstr()

--- src/libcext.c
+++ src/libcext.c
@@ -2,6 +2,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ * memtrace currently does not support IFUNC functions from libc.
+ * Instead we implement them as a workarround.
+ */
+
 char *strchr(const char *s, int c) {
     for (size_t i = 0; s[i]; i++) {
         if (s[i] == c) {
@@ -33,12 +38,39 @@ int strcmp(const char *s1, const char *s2) {
     return s1[i] - s2[i];
 }
 
+int strncmp(const char *s1, const char *s2, size_t n) {
+    size_t i = 0;
+
+    for (i = 0; s1[i] && s2[i]; i++) {
+        if (i == n) {
+            return 0;
+        }
+        if (s1[i] != s2[i]) {
+            return s1[i] - s2[i];
+        }
+    }
+
+    return s1[i] - s2[i];
+}
+
 size_t strlen(const char *s) {
     size_t i = 0;
 
     for (i = 0; s[i]; i++);
 
     return i;
+}
+
+char *strstr(const char *haystack, const char *needle) {
+    size_t i = 0;
+
+    for (i = 0; haystack[i]; i++) {
+        if (!strcmp(&haystack[i], needle)) {
+            return (char *) &haystack[i];
+        }
+    }
+
+    return NULL;
 }
 
 void *memcpy(void *dest, const void *src, size_t n) {
