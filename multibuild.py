ee09e4bb0921c502aa821541b8f2f4d115261d46
Guillaume PELLEGRINO <guillaume.pellegrino@yahoo.com>
2024-04-20 15:23:13 +0200

    Improve multibuild.py

--- multibuild.py
+++ multibuild.py
@@ -7,11 +7,19 @@
 
 import yaml
 import os
+import sys
 
 def main():
+    target = None
+    if len(sys.argv) > 1:
+        target = sys.argv[1]
+
     with open("/etc/multibuild.yaml", 'r') as stream:
         document = yaml.safe_load(stream)
+
         for key in document:
+            if target != None and target != key:
+                continue
             toolchain = document[key]
             print("Build for {} with {}".format(key, toolchain))
             os.system("O=build-{} CC={} make all -j13".format(key, toolchain))
