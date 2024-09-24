242dda24b6d00761286a52899c5c5e3e8ec6d9e6
Guillaume PELLEGRINO <guillaume.pellegrino@yahoo.com>
2024-09-24 16:02:27 +0200

    Initial aarch64 support

--- src/aarch64.c
+++ src/aarch64.c
@@ -30,12 +30,6 @@
 #include "elf.h"
 #include "log.h"
 
-typedef enum {
-    arm_cpu_mode_arm = 0,
-    arm_cpu_mode_thumb16,
-    arm_cpu_mode_thumb32,
-} arm_cpu_mode_t;
-
 typedef struct {
     long opcode;
     long size;
@@ -172,60 +166,11 @@ static void arm_prepare_function_call(cpu_registers_t *regs, int pid) {
     cpu_register_set(regs, cpu_register_sp, rsp);
 }
 
-//gdb implementation
-//calloc: 0xb6f338fd (0xb6ede000 + 0x558fd)
-//ptrace(PTRACE_PEEKTEXT, 12092, 0xb6f338fc, [0x201ea40]) = 0
-//ptrace(PTRACE_POKEDATA, 12092, 0xb6f338fc, 0xa000f7f0) = 0
-//
-//> - two-byte thumb breakpoint: 0xde01
-//> - four-byte thumb breakpoint: 0xa000f7f0
-//> - arm breakpoint: 0xe7f001f0
-static breakpoint_instr_t arm_breakpoint_instr(arm_cpu_mode_t cpu_mode) {
-    switch (cpu_mode) {
-        case arm_cpu_mode_arm:
-            return (breakpoint_instr_t) {
-                .opcode = 0xe7f001f0,
-                .size = 4,
-            };
-        case arm_cpu_mode_thumb32:
-            return (breakpoint_instr_t) {
-                .opcode = 0xa000f7f0,
-                .size = 4,
-            };
-        case arm_cpu_mode_thumb16:
-            return (breakpoint_instr_t) {
-                .opcode = 0xDE01,
-                .size = 2,
-            };
-        default:
-            abort();
-    }
-}
-
-static arm_cpu_mode_t arm_get_cpu_mode(long addr, long instr) {
-    if (!(addr & 0x01)) {
-        return arm_cpu_mode_arm;
-    }
-    if (addr & 0x02) {
-        // Instruction is not aligned on 4xbits => THUM16
-        return arm_cpu_mode_thumb16;
-    }
-    else {
-        // 16/32 bit Thumb Instruction Encoding
-        // Half Word 1 :
-        //      11100.xxxx: 16bits Thumb instruction
-        //      111xx.xxxx: 32bits Thumb-2 instruction
-        //      xxxxx.xxxx: 16bits Thumb instruction
-        long opcode = (instr & 0xF800) >> 11;
-        switch (opcode) {
-            case 0b11101:
-            case 0b11110:
-            case 0b11111:
-                return arm_cpu_mode_thumb32;
-            default:
-                return arm_cpu_mode_thumb16;
-        }
-    }
+static breakpoint_instr_t arm_breakpoint_instr() {
+    return (breakpoint_instr_t) {
+        .opcode = 0xD43E0000,
+        .size = 4,
+    };
 }
 
 static bool arm_breakpoint_set(breakpoint_t *bp, int memfd, size_t breakpoint_addr) {
@@ -239,8 +184,7 @@ static bool arm_breakpoint_set(breakpoint_t *bp, int memfd, size_t breakpoint_ad
             return false;
         }
     }
-    arm_cpu_mode_t cpu_mode = arm_get_cpu_mode(bp->addr, bp->orig_instr);
-    breakpoint_instr_t instr = arm_breakpoint_instr(cpu_mode);
+    breakpoint_instr_t instr = arm_breakpoint_instr();
 
     TRACE_LOG("Set breakpoint instr (0x%lX) at 0x%zX (old: 0x%zX)",
         instr.opcode, bp->addr, bp->orig_instr);
@@ -264,5 +208,5 @@ arch_t arch = {
     .cpu_register_reference = arm_cpu_register_reference,
     .prepare_function_call = arm_prepare_function_call,
     .breakpoint_set = arm_breakpoint_set,
-    .syscall_rewind_size = 2,
+    .syscall_rewind_size = 4,
 };
